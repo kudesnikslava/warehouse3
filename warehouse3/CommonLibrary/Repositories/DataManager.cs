9609a79b705eb09d84920c191313656d15ad6841
kudesnikslava <46519515+kudesnikslava@users.noreply.github.com>
2019-04-03 00:35:48 +0300

    Add files via upload

---
+++ warehouse3/CommonLibrary/Repositories/DataManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLibrary.Models;
+using CommonLibrary.Repositories.Implementations;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace CommonLibrary.Repositories
+{
+    public class DataManager
+    {
+	    private CustomersRepository _customersRepository;
+	    private EntitiesRepository _entitiesRepository;
+
+		private IDistributedCache _distributedCache;
+
+		public DataManager(CustomersRepository customersRepository, EntitiesRepository entitiesRepository, IDistributedCache distributedCache)
+		{
+			_customersRepository = customersRepository;
+			_entitiesRepository = entitiesRepository;
+			_distributedCache = distributedCache;
+		}
+
+	    #region Customers
+
+
+	    public List<Customer> GetAllCustomers()
+	    {
+		    return _customersRepository.GetAll().Result;
+	    }
+
+	    public Customer GetCustomerById(string customerId)
+	    {
+		    return _customersRepository.GetById(customerId).Result;
+	    }
+
+	    public async Task CreateCustomer(Customer newCustomer)
+	    {
+		    await _customersRepository.Create(newCustomer);
+	    }
+
+	    public void UpdateOrCreateCustomer(Customer customer)
+	    {
+		    _customersRepository.Replace(customer);
+	    }
+
+	    public bool RemoveCustomer(string customerId)
+	    {
+		    return _customersRepository.Remove(customerId).Result;
+	    }
+
+	    public bool CustomerExists(string customerId)
+	    {
+		    return GetCustomerById(customerId) != null;
+	    }
+
+		#endregion Customers
+
+		#region Entities
+
+
+		public List<Entity> GetAllEntities()
+	    {
+		    return _entitiesRepository.GetAll().Result;
+	    }
+
+	    public Entity GetEntityById(string entityId)
+	    {
+		    return _entitiesRepository.GetById(entityId).Result;
+	    }
+
+	    public async Task CreateEntity(Entity newEntity)
+	    {
+		    await _entitiesRepository.Create(newEntity);
+	    }
+
+	    public void UpdateOrCreateEntity(Entity entity)
+	    {
+		    _entitiesRepository.Replace(entity);
+	    }
+
+	    public bool RemoveEntity(string entityId)
+	    {
+		    return _entitiesRepository.Remove(entityId).Result;
+	    }
+
+	    public bool EntityrExists(string entityId)
+	    {
+		    return GetEntityById(entityId) != null;
+	    }
+
+	    #endregion Entities
+	}
+}
---
+++ warehouse3/Warehouse/Models/DataProvider.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonLibrary.Models;
+
+namespace Warehouse.Models
+{
+    public class DataProvider
+    {
+	    private static DataProvider _instance;
+
+		private List<Customer> _customers;
+	    private List<Entity> _entities;
+
+		public DataProvider()
+	    {
+			_customers = new List<Customer>();
+		    _entities = new List<Entity>();
+
+			InitCustomers();
+		    InitEntities();
+	    }
+
+	    public static DataProvider Instance
+	    {
+		    get
+		    {
+			    if (_instance == null)
+			    {
+				    _instance = new DataProvider();
+			    }
+
+			    return _instance;
+		    }
+	    }
+
+		#region Customers
+
+		public List<Customer> Customers
+	    {
+		    get { return _customers; }
+	    }
+
+	    public Customer GetCustomer(string customerId)
+	    {
+		    return Customers.FirstOrDefault(f => f.Id == customerId);
+	    }
+
+
+
+	    private void InitCustomers()
+	    {
+		    _customers.Add( new Customer()
+		    {
+			    Id = "C1",
+			    FirstName = "Petrov",
+			    LastName = "Petr",
+			    Age = 20,
+			    CreationDate = new DateTime(2010, 12, 03, 14, 16, 54)
+		    });
+
+		    _customers.Add(new Customer()
+		    {
+			    Id = "C2",
+			    FirstName = "Sidorov",
+			    LastName = "Sidr",
+			    Age = 25,
+			    CreationDate = new DateTime(2011, 11, 04, 11, 26, 34)
+		    });
+	    }
+
+	    public void AddCustomer(Customer customer)
+	    {
+		    _customers.Add(customer);
+	    }
+
+	    public void AddOrUpdateCustomer(Customer customer)
+	    {
+		    var index = _customers.FindIndex(f => f.Id == customer.Id);
+		    if (index < 0)
+		    {
+			    AddCustomer(customer);
+		    }
+		    else
+		    {
+			    _customers[index] = customer;
+		    }
+	    }
+
+		#endregion Customers
+
+
+	    #region Entities
+
+	    public List<Entity> Entities
+		{
+		    get { return _entities; }
+	    }
+
+	    public Entity GetEntity(string entityId)
+	    {
+		    return Entities.FirstOrDefault(f => f.Id == entityId);
+	    }
+
+
+
+	    private void InitEntities()
+	    {
+		    _entities.Add(new Entity()
+		    {
+			    Id = "E1",
+			    Name = "Book",
+			    CreatedDate = new DateTime(2010, 12, 03, 14, 16, 54),
+				AvailableQuantity = 10
+		    });
+
+		    _entities.Add(new Entity()
+		    {
+			    Id = "E2",
+			    Name = "Table",
+			    CreatedDate = new DateTime(2010, 12, 03, 14, 16, 54),
+			    AvailableQuantity = 5
+		    });
+
+		}
+
+	    public void AddEntity(Entity entity)
+	    {
+		    _entities.Add(entity);
+	    }
+
+	    public void AddOrUpdateEntity(Entity entity)
+	    {
+		    var index = _entities.FindIndex(f => f.Id == entity.Id);
+		    if (index < 0)
+		    {
+			    AddEntity(entity);
+		    }
+		    else
+		    {
+			    _entities[index] = entity;
+		    }
+	    }
+
+		#endregion Entities
+	}
+}
